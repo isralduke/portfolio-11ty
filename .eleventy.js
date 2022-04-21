3e225379d147c8aa2c45f7d1506b79660af8e453
isral Duke <isralduke@users.noreply.github.com>
2022-04-21 09:11:26 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/js/");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
   return {
     dir: {
       input: "src",
