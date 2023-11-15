978038d66d9545a7f520016afced62d381e26dc5
isral Duke <isralduke@users.noreply.github.com>
2023-11-15 16:48:03 -0600

    ongoing

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,9 @@ const eleventySass = require("eleventy-sass");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
-  eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/css/fonts/");
+  eleventyConfig.addWatchTarget("./src/styles/");
+  eleventyConfig.addPassthroughCopy("./src/styles/");
+  eleventyConfig.addPassthroughCopy("./src/styles/fonts/");
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/js/");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
